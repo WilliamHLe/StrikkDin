d0a0a0ffb4fd5ae4946226eef0798a21e99247d3
williahl <williahl@stud.ntnu.no>
2020-02-25 14:02:29 +0100

    #4 Legg til attributter på messages

--- PU13/messages1/admin.py
+++ PU13/messages1/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Messages1
 
 class MessagesAdmin(admin.ModelAdmin):
-    list_display = ('text_from', 'created_at', 'subject', 'description')
+    list_display = ('text_from', 'created_at','subject', 'description')
 
 admin.site.register(Messages1, MessagesAdmin)
