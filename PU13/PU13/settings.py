cbc4d9618ac6d4066fd9614f576ef7f583128e5b
williahl <williahl@stud.ntnu.no>
2020-02-25 12:21:59 +0100

    #17 Diverse endringer i struktur, url, design på skjema
    - Lagde egen fane for kontaktformen
    - Endret på urls
    - Endret design på registreringsskjema

--- PU13/PU13/settings.py
+++ PU13/PU13/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'accounts',
 
 
-    #'widget_tweaks',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- PU13/accounts/forms.py
+++ PU13/accounts/forms.py
@@ -4,17 +4,16 @@ from .models import CustomUser
 
 
 class MyUserCreationForm(UserCreationForm):
-
+    choi = (('Nybegynner', 'Nybegynner'), ('Erfaren', 'Erfaren'), ('Proff', 'Proff'),)
+    user_level = forms.ChoiceField(choices=choi)
     class Meta(UserCreationForm):
         model = CustomUser
         fields = ('username', 'name', 'user_level', 'is_User', 'is_Company')
 
 
 class MyUserChangeForm(UserChangeForm):
-
     class Meta(UserChangeForm):
         model = CustomUser
         fields = ('username', 'name', 'user_level', 'is_User', 'is_Company')
-
 
 
