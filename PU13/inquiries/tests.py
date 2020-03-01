b59543594f937c17512c56d9b277c82f3de8c8d4
Vivi Svendsen <vcsvends@stud.ntnu.no>
2020-03-01 15:21:20 +0100

    Created unittests and basic integration tests for the main functionalities in login, registration and sending a message to the admin.

--- PU13/inquiries/tests.py
+++ PU13/inquiries/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+from django.contrib import messages
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.test import Client
+
+from .models import Inquiries
+from .forms import SendMessageToAdmin
+
+
+class InquiriesTesting(TestCase):
+    @classmethod
+    def setUp(self):
+        self.kontakt_url = reverse("inquiries:kontakt")
+
+    def test_valid_input_test(self):
+        message_form = {
+            'text_from': 'Vivi',
+            'subject': 'Hjelp',
+            'description': 'Blah Blah Blah'
+        }
+        form = SendMessageToAdmin(data=message_form)
+        print(form.errors)
+        self.assertTrue(form.is_valid())
+
+    def test_feedback_response(self):
+        message_form = {
+            'text_from': 'Vivi',
+            'subject': 'Hjelp',
+            'description': ''
+        }
+        form = SendMessageToAdmin(data=message_form)
+        print(form.errors)
+        response = self.client.post(self.kontakt_url, message_form)
+
+        print(response.wsgi_request)
+
+       # self.assertEquals('Melding sendt!',messages.success(response, 'Melding sendt!'))
+        
+
+
+
+
+
+
