d9c5afd3ba77724d88e64e7315055dfbb2e51697
andreastorgersen <andrent@stud.ntnu.no>
2020-03-15 19:33:14 +0100

    Skrevet tester for url og views

--- PU13/inquiries/tests.py
+++ PU13/inquiries/tests.py
@@ -1,13 +1,15 @@
+import datetime
 import unittest
 
-from django.contrib import messages
-from django.test import TestCase
-from django.urls import reverse
-from django.contrib.auth.models import User
-from django.test import Client
+from django.db.models import DateField
+from django.test import TestCase, Client
+from django.urls import reverse, resolve
 
-from .models import Inquiries
+from .views import home
+
 from .forms import SendMessageToAdmin
+from .models import Inquiries
+import json
 
 
 class InquiriesTesting(TestCase):
@@ -15,6 +17,13 @@ class InquiriesTesting(TestCase):
     def setUp(self):
         self.kontakt_url = reverse("inquiries:kontakt")
 
+    # tester at url-en er riktig.
+
+    def test_kontakt_url_resolves(self):
+        url = reverse("kontakt")
+        self.assertEquals(resolve(url).func, home)
+
+    # tester at lovlig input passerxer gjennom
     def test_valid_input_test(self):
         message_form = {
             'text_from': 'Vivi',
@@ -26,10 +35,34 @@ class InquiriesTesting(TestCase):
         self.assertTrue(form.is_valid())
 
 
-        
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.kontakt_url = "/kontakt/"
+        self.melding1 = Inquiries.objects.create(
+            text_from="hejek",
+            subject="hejk",
+            description="hjklkjh",
+            created_at=0
+        )
+
+    # Tester det å fylle ut skjemaet
+    def test_inquiries_home_POST_add_new_message(self):
+        response = self.client.post(self.kontakt_url, {
+            "text_from": "hejek",
+            "subject": "hejk",
+            "description": "hjklkjh",
+            "created_at": "0"
+        })
+
+        self.assertEquals(self.melding1.text_from, "hejek")
+
+        self.assertEquals(response.status_code, 302)
 
 
 
-
-
-
+        # Under vises metoden for å teste get-request
+        # response = self.client.get(reverse(self.kontakt_url))
+        # self.assertEquals(response.status_code, 200)
+        # self.assertTemplateUsed(response, "contactAdmin.html")
