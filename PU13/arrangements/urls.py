9dfaa7da0246f377d3420d8f6d5e9d1fb74264a2
williahl <williahl@stud.ntnu.no>
2020-03-15 16:20:28 +0100

    #31 Add challenge company restriction

--- PU13/arrangements/urls.py
+++ PU13/arrangements/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("arr/", views.challengeView, name="arr"),
-    path("arr/<int:pk>/", views.challenge_detail, name="challenge_detail"),
+    path("utfordring/", views.challengeView, name="arr"),
+    path("utfordring/<int:pk>/", views.challenge_detail, name="challenge_detail"),
 
     # Link to detailed challenge view based on challenge ID
-    path("arr/create/", views.create_challenge, name="create_challenge"),
+    path("utfordring/opprett/", views.create_challenge, name="create_challenge"),
 
     path("minside/", views.my_page, name="my_page"),
     path("minside/<int:pk>/", views.deregister_challenge, name="delete"),
