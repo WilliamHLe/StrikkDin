5a30a828421a70c9b3f428e319d1872fa2346738
Mai Tran <maiht99@hotmail.com>
2020-03-19 00:41:14 +0100

    Added docs for inquiries/admin.py

--- PU13/inquiries/admin.py
+++ PU13/inquiries/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Inquiries
 
 class MessagesAdmin(admin.ModelAdmin):
+    """
+    List_display skal kunne vise felter som Admin kan se på siden sin.
+    """
     list_display = ('text_from', 'created_at','subject', 'description')
 
+"""
+Kobler Inquiries-modellen til MessagesAdmin-klassen.
+"""
 admin.site.register(Inquiries, MessagesAdmin)
