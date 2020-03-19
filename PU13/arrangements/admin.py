3d6ef15d6e7a1ebd259897d7a5b0659c8494bc5b
williahl <williahl@stud.ntnu.no>
2020-03-20 00:01:31 +0100

    #36 Create yarn model

--- PU13/arrangements/admin.py
+++ PU13/arrangements/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Challenge, KnitNight
+from .models import Challenge, KnitNight, Ads
 
 
 class ChallengeAdmin(admin.ModelAdmin):
@@ -10,5 +10,10 @@ class KnitAdmin(admin.ModelAdmin):
     list_display = ('knit_name', 'time', 'created_at', 'created_by')
 
 
+class YarnAdmin(admin.ModelAdmin):
+    list_display = ('yarn_name', 'created_at', 'created_by')
+
+
+admin.site.register(Ads, YarnAdmin)
 admin.site.register(KnitNight, KnitAdmin)
 admin.site.register(Challenge, ChallengeAdmin)
--- PU13/arrangements/urls.py
+++ PU13/arrangements/urls.py
@@ -12,6 +12,11 @@ urlpatterns = [
     path("strikkekveld/opprett/", views.create_knit, name="create_knit"),
     path("strikkekveld/<int:pk>/", views.knit_detail, name="knit_detail"),
 
+    # Link for knit views
+    path("annonse/", views.yarnView, name="yarn"),
+    path("annonse/opprett/", views.create_yarn, name="create_yarn"),
+    # path("annonse/<int:pk>/", views.knit_detail, name="knit_detail"),
+
     # Link for my page
     path("minside/", views.my_page, name="my_page"),
     path("minside/avmeld/utfordring/<int:pk>/", views.deregister_challenge, name="delete"),
