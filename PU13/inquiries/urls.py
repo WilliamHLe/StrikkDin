a1d0e7a469e39114e40e1b1bdcb864d38132d5a9
Mai Tran <maiht99@hotmail.com>
2020-03-19 02:08:16 +0100

    Added comment to inquiries/urls.py Also tweaked urlpatterns

--- PU13/inquiries/urls.py
+++ PU13/inquiries/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import home
 
-# Kobler viewet home til en URL og gir ID-en 'kontakt'. 
+# Kobler viewet home til en URL og gir ID-en 'kontakt'.
 urlpatterns = [
     path("kontakt/", home, name ="kontakt")
 ]
