d8271d1f6f908b298f060eae7a00bbdebc660fde
Mai Tran <maiht99@hotmail.com>
2020-03-19 01:33:13 +0100

    Added docs to inquiries/models .py

--- PU13/inquiries/models.py
+++ PU13/inquiries/models.py
@@ -2,12 +2,23 @@ from django.db import models
 
 
 class Inquiries(models.Model):
+    """
+    En generell modell som består av ulike meldings-felter som representeres i databasen
+    Null=True tillater at det kan representeres som NULL i databasen.
+    auto_now=True viser den dato og tid meldingen ble sendt.
+    """
     text_from = models.CharField(max_length=100, null=True)
     subject = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
     created_at = models.DateField(auto_now=True)
 
     class Meta:
+        """
+        Ordering er en av blant flere "Meta-options" som Django tilbyr.
+        Den er reservert til sortering. I dette tilfelle sorterer den meldingene i stigende rekkefølge.
+        "-" som prefiks, altså ['-created-at'] vil den sortere meldingene i synkende rekkefølge, uten "-" sorteres det stigende.
+        "?" som prefiks vil det sorteres tilfeldig.
+        """
         ordering = ['created_at']
 
     def __str__(self):
