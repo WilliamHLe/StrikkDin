4c17d6f564650f128b7edff17e8792f17d89f3b3
Mai Tran <maiht99@hotmail.com>
2020-03-21 14:27:07 +0100

    Added form fields with some attributes to make it easier to render in create_blog.html

--- PU13/personal_feed/forms.py
+++ PU13/personal_feed/forms.py
@@ -4,6 +4,12 @@ from .models import Personal_Feed_Post
 
 
 class Create_Personal_Feed_Post_Form(forms.ModelForm):
+    title = forms.CharField(label="Tittel",
+                            widget=forms.TextInput(attrs={'placeholder': 'Tittel'}),
+                            )
+    content = forms.CharField(label="Emne",
+                              widget=forms.Textarea(attrs={'placeholder': 'Dette innlegget er om ..'}),
+                            )
 
     class Meta:
         model = Personal_Feed_Post
