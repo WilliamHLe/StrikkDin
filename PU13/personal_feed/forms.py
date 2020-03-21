7d0500317f7f75559787c9a4d2b6ee852946d224
Mai Tran <maiht99@hotmail.com>
2020-03-21 14:35:55 +0100

    Changed label from 'Emne' to 'Innhold'

--- PU13/personal_feed/forms.py
+++ PU13/personal_feed/forms.py
@@ -7,7 +7,7 @@ class Create_Personal_Feed_Post_Form(forms.ModelForm):
     title = forms.CharField(label="Tittel",
                             widget=forms.TextInput(attrs={'placeholder': 'Tittel'}),
                             )
-    content = forms.CharField(label="Emne",
+    content = forms.CharField(label="Innhold",
                               widget=forms.Textarea(attrs={'placeholder': 'Dette innlegget er om ..'}),
                             )
 
