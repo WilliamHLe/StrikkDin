95fc51e57f0c0dafbebbcf05a80d094a8c7a4f00
williahl <williahl@stud.ntnu.no>
2020-06-15 00:20:53 +0200

    Deploying Heroku

--- PU13/PU13/settings.py
+++ PU13/PU13/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p4=q9#72%=p27qc_278knk8&@exn+2@@tdv1fmg+a9@ev^ywr('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['strikkdin.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
